b0a88054f09681aa900ada71d7e573a5d1431292
mastabigmatt <michaelgueye@gmail.com>
2017-09-27 14:51:24 +0200

    game constants

---
+++ core/src/GameConstants/Constants.java
@@ -0,0 +1,12 @@
+package GameConstants;
+
+public class Constants {
+    public static final int sc_Height = 480;
+    public static final int sc_Width = 640;
+    public static final int centerX = sc_Width/2;
+    public static final int centerY = sc_Height/2;
+    public static final int col_width = sc_Width/8;
+    public static final int row_height = sc_Height/8;
+    public static final String skin = "skin/glassy-ui.json";
+    public static final String skinAtlas = "skin/glassy-ui.atlas";
+}
