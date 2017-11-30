37059efe3e9bf6056adf715ce9aca7cc12f15546
Andrew <awrgold@gmail.com>
2017-11-30 11:32:28 +0100

    Greedy Algo v2 stash

---
+++ core/src/GameAI/Greedy.java
@@ -0,0 +1,66 @@
+package GameAI;
+
+import com.game.*;
+
+
+public class Greedy implements GreedySearch {
+
+    public Action getNextMove(GameState state){
+        /*
+        - If start of game (3+ lowest colors):
+        --- Prioritize double tile
+        --- If no double, find tiles with lowest scores
+        --- Try placements that increase score
+        --- Return Action that maximizes score (hex 1 > hex 2 in tile)
+
+        - If AI has 2 lowest colors:
+        --- Prioritize tiles with both colors
+        --- If not, choose tile with random lowest color
+        --- Try placements that increase score
+        --- Return Action that maximizes score
+
+        - If AI has 1 lowest color:
+        --- Prioritize double color tiles
+        --- If not, choose tile with lowest color + 2nd (or 3rd or 4th) lowest color
+        --- Try placements that increase score
+        --- Prioritize placements that lead to ingenious
+        --- Return Action that maximizes score
+         */
+
+        Player nowPlaying = state.getGamingPlayer();
+
+        if(state.getGamingPlayer().howManyLowestColors() > 2){
+            /*
+            1) Search hand
+            2) Find all tiles with lowest colors
+            3) Place them in a priority queue, with any double tiles or tiles with both lowest colors at top of queue
+            5) Evaluate each tile in the queue for best scoring move
+            6) Choose tile with best score, or at random if score is same (can update to predict future moves)
+            7) Return action placing that tile
+             */
+        }
+
+        if(state.getGamingPlayer().howManyLowestColors() == 2){
+            /*
+            1) Search hand
+            2) Find both tiles with lowest colors
+            3) Place them in a priority queue, with any double tiles or tiles with both lowest colors at top of queue
+            5) Evaluate each tile in the queue for best scoring move
+            6) Choose tile with best score, or at random if score is same (can update to predict future moves)
+            7) Return action placing that tile
+             */
+        }
+
+        if(state.getGamingPlayer().howManyLowestColors() < 2){
+            /*
+            1) Search hand
+            2) Find any tiles with lowest colors
+            3) Place them in a priority queue, with any double tiles or tiles with lower scoring colors at top of queue
+            5) Evaluate each tile in the queue for best scoring move
+            6) Choose tile with best score, or at random if score is same (can update to predict future moves)
+            7) Return action placing that tile
+             */
+        }
+
+    }
+}
---
+++ core/src/GameAI/GreedySearch.java
@@ -0,0 +1,4 @@
+package GameAI;
+
+public interface GreedySearch {
+}
